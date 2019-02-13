6dae66092a262f81db0b02275619d2b69f8b5dd4
RachelKolk <rach.m.obrien@gmail.com>
2019-02-13 17:31:48 -0500

    all posts router CRUD requests coded and working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,6 +4,8 @@ const PostData = require('../data/helpers/postDb.js');
 
 const router = express.Router();
 
+
+// GETS/READS all of the posts 
 router.get('/', async (req, res) => {
     try {
         const posts = await PostData.get(req.query);
@@ -13,6 +15,8 @@ router.get('/', async (req, res) => {
     }
 });
 
+
+// GETS/READS the post by the ID of the post (not the ID of the user)
 router.get('/:id', async (req, res) => {
     try {
         const posts = await PostData.getById(req.params.id);
@@ -28,6 +32,8 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+
+// CREATES a new post
 router.post('/', async (req, res) => {
     try {
         const post = await PostData.insert(req.body);
@@ -38,9 +44,40 @@ router.post('/', async (req, res) => {
     }
 });
 
-// router.update('/')
 
-// router.delete
+// UPDATES a post by that post's ID (not by the user's ID)
+router.put('/:id', async (req, res) => {
+    if (req.body.user_id == null || req.body.text == null) {
+        res.status(400).json({errorMessage: "Please provide a user ID and text"});
+    }
+    try {
+        const post = await PostData.update(req.params.id, req.body);
+        if (post == 0) {
+            res.status(404).json({message: "The post with that ID cannot be found."});
+        } else {
+            res.status(200).json(req.body);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "The post could not be modified"});
+    } 
+});
+
+
+// DELETES a post - uses the post ID to do so 
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteCount = await PostData.remove(req.params.id);
+        if (deleteCount > 0) {
+            res.status(200).json({message: "The post has been deleted"});
+        } else {
+            res.status(404).json({message: "That post could not be found"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "There was an error while deleting th post"});
+    }
+});
 
 
 
