76e29b5f92c4c256064e369c61b92b8f1134bf75
RachelKolk <rach.m.obrien@gmail.com>
2019-02-13 18:15:53 -0500

    update, delete and create working on user router

--- users/users-router.js
+++ users/users-router.js
@@ -4,6 +4,8 @@ const UserData = require('../data/helpers/userDb.js');
 
 const router = express.Router();
 
+
+// GETS/READS all of the users
 router.get('/', async (req, res) => {
     try {
         const users = await UserData.get(req.query);
@@ -13,6 +15,8 @@ router.get('/', async (req, res) => {
     }  
 });
 
+
+// GETS/READS all of the users by ID
 router.get('/:id', async (req, res) => {
     try {
         const user = await UserData.getById(req.params.id);
@@ -27,10 +31,68 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// router.insert('/', async (req, res) => {
-//     try {
-        
-//     }
-// })
+
+// GETS/READS all of the posts made by a particular user 
+router.get('/:id/posts', async (req, res) => {
+    try {
+        const posts = await UserData.getUserPosts(req.params.id);
+        if (posts) {
+            res.status(200).json(posts)
+        } else {
+            res.status(404).json({message: "Posts by that user can not be found"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "User can not be found"});
+    }
+});
+
+
+// CREATES a new user
+router.post('/', async (req, res) => {
+    if (req.body.name == "") {
+        res.status(400).json({errorMessage: "Please provide a username"});
+    }
+    try {
+       const user = await UserData.insert(req.body);
+       res.status(201).json(user)
+    } catch {
+        console.log(error);
+        res.status(500).json({message: "Error adding user"});
+    }
+});
+
+
+//UPDATES a user
+router.put('/:id', async (req, res) => {
+    try {
+        const user = await UserData.update(req.params.id, req.body);
+        if (user == 0) {
+            res.status(404).json({message: "That user can not be found"});
+        } else {
+            res.status(200).json(user);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "The user could not be updated"});
+    }
+});
+
+
+//DELETES a user
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteCount = await UserData.remove(req.params.id);
+        if (deleteCount > 0) {
+            res.status(200).json({message: "This user has been deleted"});
+        } else {
+            res.status(404).json({message: "The user could not be found"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "An error occurred while deleting the user."});
+    }
+});
+
 
 module.exports = router;
