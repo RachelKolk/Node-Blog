cf9ddb8672ab5a4158e02bfd4cf56b53febc1236
RachelKolk <rach.m.obrien@gmail.com>
2019-02-14 16:35:36 -0500

    changed scripts start in package.json to be node and created dynamic port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // code away!
+require('dotenv').config();
+
 const server = require('./server.js');
 
-server.listen(5000, () => {
-    console.log('Server running on Port 5000');
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localthost:${port} ***\n`);
 })
